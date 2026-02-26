64e7465ac26b8675be7f167c54606d5dfe1d84c5
Kennedy Martins <151695587+YoTwinKash@users.noreply.github.com>
2026-02-26 10:28:58 -0300

    Add files via upload

---
+++ controleEstoque/controleEstoque/src/main/java/com/kennedy/controleEstoque/ControleEstoqueApplication.java
@@ -0,0 +1,13 @@
+package com.kennedy.controleEstoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleEstoqueApplication.class, args);
+	}
+
+}
